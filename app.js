da3447a6cbafae22d14eb4750b2a8cdde83e76e8
Richard Widjaya <ricwidjaya@gmail.com>
2021-11-11 15:52:13 +0800

    Add show pages, search feature

--- app.js
+++ app.js
@@ -8,15 +8,46 @@ const exphbs = require('express-handlebars')
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+// Load restaurant data
+const restaurants = require('./restaurant.json').results
+
 // Set static files
 app.use(express.static('public'))
 
 
-// Initialize Project
+// Landing Page
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index', {
+    style: 'index.css',
+    restaurant: restaurants
+  })
 })
 
+// Restaurant info
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id
+  const restaurant = restaurants.find(restaurant => restaurant.id.toString() === id)
+
+  res.render('show', {
+    style: 'show.css',
+    restaurant: restaurant
+  })
+})
+
+// Search page
+app.get('/search', (req, res) => {
+  const keyword = req.query.keyword.toLowerCase()
+  const filteredRestaurant = restaurants.filter(restaurant => restaurant.name.toLowerCase().includes(keyword))
+
+  res.render('index', {
+    style: 'index.css',
+    restaurant: filteredRestaurant,
+    keyword: keyword
+  })
+})
+
+
+
 app.listen(port, () => {
-  console.log('It\'s a sood start')
+  console.log('Server Started')
 })
