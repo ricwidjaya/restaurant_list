42d28ff65ef56441ca50653cfd43a1147f5185b8
Richard Widjaya <ricwidjaya@gmail.com>
2021-12-06 19:05:12 +0800

    feat: add google map api

--- public/javascripts/create.js
+++ public/javascripts/create.js
@@ -1,6 +1,6 @@
 const stars = document.querySelectorAll('.fa-star')
-
-console.log(stars)
+const address = document.querySelector('#address')
+const googleMap = document.querySelector('#google-map')
 
 stars.forEach(star => {
   star.addEventListener('click', (event) => {
@@ -8,6 +8,13 @@ stars.forEach(star => {
     clearStars()
     renderStars(value)
   })
+})
+
+googleMap.src = `https://www.google.com/maps/embed/v1/search?q=taiwan&key=AIzaSyBkYPEMIAjDcR08X2DgGoB-jUViPa4_L2s`
+
+address.addEventListener('keyup', (event) => {
+  const query = event.target.value
+  googleMap.src = `https://www.google.com/maps/embed/v1/search?q=${query}&key=AIzaSyBkYPEMIAjDcR08X2DgGoB-jUViPa4_L2s`
 })
 
 
