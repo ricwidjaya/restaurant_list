0905fe1c0c2c1f674a3c4b3da8b27fecb2aaf5e1
Richard Widjaya <ricwidjaya@gmail.com>
2021-12-11 19:52:28 +0800

    feat: add sort feature

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,23 +1,67 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
 
-const Restaurant = require('../../models/restaurant')
+const Restaurant = require("../../models/restaurant")
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
+  const sortAToZ = req.query.sort
+  if (sortAToZ === "0") {
+    return Restaurant.find()
+      .lean()
+      .collation({ locale: "en_US", strength: 1 })
+      .sort({ name: 1 })
+      .then((restaurants) => {
+        res.render("index", {
+          style: "index.css",
+          script: "index.js",
+          restaurant: restaurants
+        })
+      })
+  }
+  if (sortAToZ === "1") {
+    return Restaurant.find()
+      .lean()
+      .collation({ locale: "en_US", strength: 1 })
+      .sort({ name: -1 })
+      .then((restaurants) => {
+        res.render("index", {
+          style: "index.css",
+          script: "index.js",
+          restaurant: restaurants
+        })
+      })
+  }
+
+  // By category
+  if (sortAToZ === "category") {
+    return Restaurant.find()
+      .lean()
+      .collation({ locale: "zh_Hant", strength: 1 })
+      .sort({ category: 1 })
+      .then((restaurants) => {
+        res.render("index", {
+          style: "index.css",
+          script: "index.js",
+          restaurant: restaurants
+        })
+      })
+  }
+
+  // Regular index page
   Restaurant.find()
     .lean()
-    .sort({ _id: 'desc' }) // new restaurant shows first
-    .then(restaurants => {
+    .sort({ _id: "desc" }) // new restaurant shows first
+    .then((restaurants) => {
       // Change MongoDB ObjectId into string
-      restaurants.forEach(restaurant => {
+      restaurants.forEach((restaurant) => {
         restaurant._id = restaurant._id.toString()
       })
       return restaurants
     })
     .then((restaurants) => {
-      res.render('index', {
-        style: 'index.css',
-        script: 'index.js',
+      res.render("index", {
+        style: "index.css",
+        script: "index.js",
         restaurant: restaurants
       })
     })
@@ -27,17 +71,16 @@ router.get('/', (req, res) => {
 })
 
 // Live Search API
-router.get('/search', (req, res) => {
+router.get("/search", (req, res) => {
   const keyword = req.query.keyword.toLowerCase()
 
   // Search keyword from DB with regex
-  Restaurant
-    .find({
-      $or: [
-        { name: new RegExp(keyword, 'i') },
-        { category: new RegExp(keyword, 'i') }
-      ]
-    })
+  Restaurant.find({
+    $or: [
+      { name: new RegExp(keyword, "i") },
+      { category: new RegExp(keyword, "i") }
+    ]
+  })
     .lean()
     .then((restaurants) => {
       res.json(restaurants)
