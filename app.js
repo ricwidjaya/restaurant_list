75b92feeccf849bb44dddf55434e25c8ea44c5ad
Richard Widjaya <ricwidjaya@gmail.com>
2022-01-03 23:44:50 +0800

    feat: add dotenv

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
 // Import express
-const express = require('express')
+const express = require("express")
+
+// env
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config()
+}
+
 const app = express()
-const methodOverride = require('method-override')
-const port = 3000
-require('./config/mongoose')
-const routes = require('./routes')
+const methodOverride = require("method-override")
+require("./config/mongoose")
+const routes = require("./routes")
 
 // Method Override
-app.use(methodOverride('_method'))
+app.use(methodOverride("_method"))
 
 // Set template engine to "Handlebars"
-const exphbs = require('express-handlebars')
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+const exphbs = require("express-handlebars")
+app.engine("handlebars", exphbs())
+app.set("view engine", "handlebars")
 
 // Set body parser
 app.use(express.urlencoded({ extended: true }))
 
 // Set static files
-app.use(express.static('public'))
+app.use(express.static("public"))
 
 // Routers
 app.use(routes)
 
-app.listen(port, () => {
-  console.log('Server Started')
+app.listen(process.env.PORT, () => {
+  console.log("Server Started")
 })
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,21 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose")
 
 // Connect to mongoDB
-const MONGODB_URI = process.env.MONGODB_URL ||
-  'mongodb://localhost/restaurant-list'
-mongoose.connect(MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URL)
 
 const db = mongoose.connection
 
-
-db.on('error', () => {
-  console.log('Failed to Connect MongoDB')
+db.on("error", () => {
+  console.log("Failed to Connect MongoDB")
 })
 
-
-db.once('open', () => {
-  console.log('MongoDB Connected!')
+db.once("open", () => {
+  console.log("MongoDB Connected!")
 })
-
 
 module.exports = db
